c14f531154e8307d17a0f2c670060a52d372f13d
rsbertoa90 <rsbertoa90@gmail.com>
2018-08-30 18:29:27 -0300

    modal  login box

--- public/js/app.js
+++ public/js/app.js
@@ -98835,10 +98835,43 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    props: ['list']
+    props: ['list'],
+    data: function data() {
+        return {
+            method: 'mercadopago'
+        };
+    },
 
+    computed: {
+        total: function total() {
+            var tot = 0;
+            this.list.foreach(function (inscription) {
+                tot += inscription.price;
+            });
+            return tot;
+        }
+    }
 });
 
 /***/ }),
@@ -98849,9 +98882,55 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_vm._v("\n    " + _vm._s(_vm.list) + "\n")])
+  return _c("div", [
+    _c("div", [
+      _c("table", { staticClass: "table table-striped" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c(
+          "tbody",
+          _vm._l(_vm.list, function(inscription) {
+            return _c("tr", { key: inscription.id }, [
+              _c("td", [_vm._v(" " + _vm._s(inscription.seminar.title) + " ")]),
+              _vm._v(" "),
+              _c("td", [
+                _vm._v(
+                  " " +
+                    _vm._s(inscription.date) +
+                    " - " +
+                    _vm._s(inscription.hour) +
+                    " "
+                )
+              ]),
+              _vm._v(" "),
+              _c("td", [_vm._v(" $" + _vm._s(inscription.price) + " ")])
+            ])
+          })
+        ),
+        _vm._v(" "),
+        _c("tfoot", [
+          _c("td"),
+          _vm._v(" "),
+          _c("td", [_vm._v("TOTAL")]),
+          _vm._v(" "),
+          _c("td", [_vm._v(" " + _vm._s(_vm.total) + " ")])
+        ])
+      ])
+    ])
+  ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("thead", [
+      _c("th", [_vm._v(" - ")]),
+      _vm._v(" "),
+      _c("th", [_vm._v(" - ")])
+    ])
+  }
+]
 render._withStripped = true
 module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
--- resources/assets/js/components/users/checkout/Checkout.vue
+++ resources/assets/js/components/users/checkout/Checkout.vue
@@ -1,13 +1,46 @@
 <template>
     <div>
-        {{list}}
+        <div>
+            <table class="table table-striped">
+                <thead>
+                    <th> - </th>
+                    <th> - </th>
+                </thead>
+                <tbody>
+                    <tr v-for="inscription in list" :key="inscription.id">
+                        <td> {{inscription.seminar.title}} </td>
+                        <td> {{inscription.date}} - {{inscription.hour}} </td>
+                        <td> ${{inscription.price}} </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <td></td>
+                    <td>TOTAL</td>
+                    <td> {{total}} </td>
+                </tfoot>
+            </table>
+        </div>
     </div>    
 </template>
 
 <script>
 export default {
     props:['list'],
-
+    data(){
+        return{
+            method : 'mercadopago'
+        }
+    },
+    computed:{
+        total()
+        {
+            var tot = 0;
+            this.list.foreach(inscription => {
+                tot += inscription.price;
+            });
+            return tot;
+        }
+    }
 }
 </script>
 
