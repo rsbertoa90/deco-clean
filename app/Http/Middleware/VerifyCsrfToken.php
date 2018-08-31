1f3ab163747766f2b220990960e0a3c3efb3187b
rsbertoa90 <rsbertoa90@gmail.com>
2018-08-31 18:57:11 -0300

    mp testing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/mplistener',
-        '/api/mplistener'
+        '/api/mplistener',
+        'mp/notification'
     ];
 }
