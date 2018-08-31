bf3eaf79063ddb90bd83b2530c39c359a08bddae
rsbertoa90 <rsbertoa90@gmail.com>
2018-08-31 14:21:15 -0300

    mp testing

--- public/js/app.js
+++ public/js/app.js
@@ -98773,7 +98773,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony default export */ __webpack_exports__["default"] = ({
     computed: {
         user: function user() {
-            return this.$store.getters.getUser();
+            return this.$store.getters.getUser;
         }
     },
     created: function created() {
--- resources/assets/js/components/users/checkout/MP.vue
+++ resources/assets/js/components/users/checkout/MP.vue
@@ -47,7 +47,7 @@
 export default {
     computed:{
         user(){
-            return this.$store.getters.getUser();
+            return this.$store.getters.getUser;
         }
     },
     created() {
