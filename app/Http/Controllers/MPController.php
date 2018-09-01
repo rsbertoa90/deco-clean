e3a00f1f7cde1e4f1a1ffe49c4db63ad02ad1b91
rsbertoa90 <rsbertoa90@gmail.com>
2018-09-01 20:41:15 -0300

    mp testing

--- app/Http/Controllers/MPController.php
+++ app/Http/Controllers/MPController.php
@@ -29,7 +29,7 @@ class MPController extends Controller
         $item->title = "Sleek Rubber Bag";
         $item->quantity = 1;
         $item->currency_id = "ARS";
-        $item->unit_price = 10;
+        $item->unit_price = 50;
         # Create a payer object
         $payer = new MercadoPago\Payer();
         $payer->email = "test_user_62611257@testuser.com";
